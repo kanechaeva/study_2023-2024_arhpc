bad7b3a005b96689d52a36fabe90cfb03404a60b
Kira Nechaeva <kirusya1234@gmail.com>
2023-10-28 10:47:39 +0300

    feat(main): add files lab-4

---
+++ labs/lab04/hello.asm
@@ -0,0 +1,19 @@
+; hello.asm
+SECTION .data 			     ; Начало секции данных
+	hello: 	DB 'Hello world!',10 ; 'Hello world!' плюс
+                                     ; символ перевода строки
+	helloLen:  EQU $-hello       ; Длина строки hello
+
+SECTION .text          		; Начало секции кода
+	GLOBAL _start
+
+_start:                   	; Точка входа в программу
+	mov eax,4 		; Системный вызов для записи (sys_write)
+	mov ebx,1 		; Описатель файла '1' - стандартный вывод
+	mov ecx,hello 		; Адрес строки hello в ecx
+	mov edx,helloLen 	; Размер строки hello
+	int 80h 		; Вызов ядра
+	
+	mov eax,1 		; Системный вызов для выхода (sys_exit)
+	mov ebx,0 		; Выход с кодом возврата '0' (без ошибок)
+	int 80h 		; Вызов ядра
